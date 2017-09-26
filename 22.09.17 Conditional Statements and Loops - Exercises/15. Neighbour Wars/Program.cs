ecf4bcec9ff648e72f8b55936e0be5eac69060fe
daqn55 <daqn_5@abv.bg>
2017-09-26 22:51:56 +0300

    update homework

--- 22.09.17 Conditional Statements and Loops - Exercises/15. Neighbour Wars/Program.cs
+++ 22.09.17 Conditional Statements and Loops - Exercises/15. Neighbour Wars/Program.cs
@@ -13,7 +13,48 @@ namespace _15.Neighbour_Wars
             int peshoDamage = int.Parse(Console.ReadLine());
             int goshoDamage = int.Parse(Console.ReadLine());
 
+            int peshoHealth = 100;
+            int goshoHealth = 100;
+            int count = 0;
 
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (peshoHealth <= 0)
+                {
+                    Console.WriteLine($"Gosho won in {count}th round.");
+                    break;
+                }
+                else if (goshoHealth <= 0)
+                {
+                    Console.WriteLine($"Pesho won in {count}th round.");
+                    break;
+                }
+                else
+                {
+                    if (i % 2 != 0)
+                    {
+                        goshoHealth -= peshoDamage;
+                        if (!(goshoHealth <= 0))
+                        {
+                            Console.WriteLine($"Pesho used Roundhouse kick and reduced Gosho to {goshoHealth} health.");
+                        }
+                    }
+                    else
+                    {
+                        peshoHealth -= goshoDamage;
+                        if (!(peshoHealth <= 0))
+                        {
+                            Console.WriteLine($"Gosho used Thunderous fist and reduced Pesho to {peshoHealth} health.");
+                        }
+                    }
+                    if (i % 3 == 0 && goshoHealth > 0 && peshoHealth > 0)
+                    {
+                        peshoHealth += 10;
+                        goshoHealth += 10;
+                    }
+                }
+                count++;
+            }
         }
     }
 }
