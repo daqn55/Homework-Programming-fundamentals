fececf6b4d529e2067776833bfe52e413805185d
daqn55 <daqn_5@abv.bg>
2017-09-26 18:26:52 +0300

    Intro and Basic Syntax - Exercises - 19-09-17

---
+++ Intro and Basic Syntax - Exercises - 19-09-17/02 - Rectangle Area/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02___Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double area = w * h;
+
+            Console.WriteLine($"{area:f2}");
+        }
+    }
+}
---
+++ Intro and Basic Syntax - Exercises - 19-09-17/05. Character Stats/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Character_Stats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currentHealth = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.Write("Health: ");
+            Console.Write("|");
+            if (currentHealth == 0)
+            {
+                Console.Write(new string('.', maxHealth));
+            }
+            else
+            {
+                int oddHealth = maxHealth - currentHealth;
+                Console.Write(new string('|', currentHealth));
+                Console.Write(new string('.', oddHealth));
+            }
+            Console.WriteLine("|");
+
+            Console.Write("Energy: ");
+            Console.Write("|");
+            if (currentEnergy == 0)
+            {
+                Console.Write(new string('.', maxEnergy));
+            }
+            else
+            {
+                int oddEnergy = maxEnergy - currentEnergy;
+                Console.Write(new string('|', currentEnergy));
+                Console.Write(new string('.', oddEnergy));
+            }
+            Console.WriteLine("|");
+
+        }
+    }
+}
