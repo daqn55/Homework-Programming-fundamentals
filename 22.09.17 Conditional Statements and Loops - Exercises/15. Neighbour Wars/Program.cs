f5c6e7ad7301a334d62bfbdf36c446dfa4f50d4c
daqn55 <daqn_5@abv.bg>
2017-09-26 19:40:43 +0300

    update homework

---
+++ 22.09.17 Conditional Statements and Loops - Exercises/15. Neighbour Wars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.Neighbour_Wars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peshoDamage = int.Parse(Console.ReadLine());
+            int goshoDamage = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
