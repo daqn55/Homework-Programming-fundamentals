505d64202f044d7002c403df02023d7be64acb78
daqn55 <daqn_5@abv.bg>
2017-09-26 18:29:17 +0300

    21.09.17 Conditional Statements and Loops - Lab

---
+++ 21.09.17 Conditional Statements and Loops - Lab/08. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Sum_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int n = 1;
+            int sum = 0;
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine(n);
+                sum += n;
+                n += 2;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
