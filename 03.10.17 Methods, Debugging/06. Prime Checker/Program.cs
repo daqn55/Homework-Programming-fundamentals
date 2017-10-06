b333f435abda27c7ba212ee8b17ce0461462656b
daqn <daqn_5@abv.bg>
2017-10-06 23:17:35 +0300

    update Homework

--- 03.10.17 Methods, Debugging/06. Prime Checker/Program.cs
+++ 03.10.17 Methods, Debugging/06. Prime Checker/Program.cs
@@ -4,9 +4,39 @@ namespace _06._Prime_Checker
 {
     class Program
     {
+        static bool IsPrime(long num)
+        {
+            int count = 0;
+            for (long i = 1; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+                if (count > 2)
+                {
+                    break;
+                }
+            }
+            if (num == 0 || num == 1)
+            {
+                return false;
+            }
+            else if (count == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            long num = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(IsPrime(num));
         }
     }
 }
