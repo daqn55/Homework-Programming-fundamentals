1fcb0f02e4780b8b3928bd036f7ba4f0ac565127
daqn <daqn_5@abv.bg>
2017-10-06 19:26:08 +0300

    Homework

---
+++ 03.10.17 Methods, Debugging/06. Prime Checker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06._Prime_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
