6316960404f08c73757a88a61a1aa42e73c6efaf
daqn <daqn_5@abv.bg>
2017-10-07 18:55:27 +0300

    update homework

---
+++ 03.10.17 Methods, Debugging/07. Primes in Given Range/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Primes_in_Given_Range
+{
+    class Program
+    {
+        static List<int> FindPrimesInRange(int startNum, int endNum)
+        {
+            List<int> list = new List<int>();
+            if (startNum == 0 || startNum == 1)
+            {
+                startNum = 2;
+            }
+            for (int i = startNum; i <= endNum; i++)
+            {
+                int count = 0;
+                int saveCurrentNum = 0;
+                for (int y = 2; y <= i; y++)
+                {
+                    if (i % y == 0)
+                    {
+                        count++;
+                    }
+                    if (count > 2)
+                    {
+                        break;
+                    }
+                    saveCurrentNum = y;
+                }
+                if (count == 1)
+                {
+                    list.Add(saveCurrentNum);
+                }
+            }
+            return list;
+        }
+        static void Main(string[] args)
+        {
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+            if (endNum <= startNum)
+            {
+                Console.WriteLine("(empty list)");
+            }
+            else
+            {
+                List<int> list = FindPrimesInRange(startNum, endNum);
+                foreach (int prime in list)
+                {
+                    Console.Write(prime);
+                    if (prime != list.Last())
+                    {
+                        Console.Write(", ");
+                    }
+                }
+                Console.WriteLine();
+            } 
+        }
+    }
+}
---
+++ 03.10.17 Methods, Debugging/14. Factorial Trailing Zeroes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace _13._Factorial
+{
+    class Program
+    {
+        static int CountZerosFactorial(BigInteger num)
+        {
+            int count = 0;
+            for (int i = 0; i <= num; i++)
+            {
+                if (num % 10 == 0)
+                {
+                    count++;
+                    num /= 10;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine(CountZerosFactorial(result));
+        }
+    }
+}
