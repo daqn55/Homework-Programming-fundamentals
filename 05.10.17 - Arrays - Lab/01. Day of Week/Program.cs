0d6d93242b141ab37b754822041feb2036ec369a
daqn <daqn_5@abv.bg>
2017-10-07 23:12:59 +0300

    Lab exercises

---
+++ 05.10.17 - Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Day_of_Week
+{
+    class Program
+    {
+        static string DayOfWeek(int n)
+        {
+            string[] weekDays = {"Monday", "Tuesday", "Wednesday", "Thursday",
+            "Friday", "Saturday", "Sunday"};
+            string currentDay = weekDays[n];
+            return currentDay;
+
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n >= 1 && n <= 7)
+            {
+                Console.WriteLine(DayOfWeek(n - 1));
+            }
+            else
+            {
+                Console.WriteLine("Invalid Day!");
+            }
+        }
+    }
+}
---
+++ 05.10.17 - Arrays - Lab/02. Reverse Array of Integers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02._Reverse_Array_of_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                arr[i] = num;
+            }
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
