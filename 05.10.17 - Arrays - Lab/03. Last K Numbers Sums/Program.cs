079c2d9f5a0b48f462431767f510ecc131fbc2c0
daqn <daqn_5@abv.bg>
2017-10-08 19:37:59 +0300

    Lab

---
+++ 05.10.17 - Arrays - Lab/03. Last K Numbers Sums/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _03._Last_K_Numbers_Sums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            long[] seq = new long[n];
+
+            seq[0] = 1;
+            for (int i = 1; i < n; i++)
+            {
+                long sum = 0;
+                if (i < k)
+                {
+                    for (int y = k - 1; y >= 0; y--)
+                    {
+                        sum = sum + seq[y];
+                        seq[i] = sum;
+                    }
+                }
+                else
+                {
+                    for (int y = i; y >= i - k; y--)
+                    {
+                        sum = sum + seq[y];
+                        seq[i] = sum;
+                    }
+                }
+
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{seq[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
