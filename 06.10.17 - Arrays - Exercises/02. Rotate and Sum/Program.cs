96518cbefd01ba5b52d671dea94ba7ff9ab60e19
daqn <daqn_5@abv.bg>
2017-10-11 01:31:07 +0300

    homework

---
+++ 06.10.17 - Arrays - Exercises/02. Rotate and Sum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Rotate_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nIntegers = Console.ReadLine();
+            int k = int.Parse(Console.ReadLine());
+
+            string[] NumsInString = nIntegers.Split(' ');
+            int[] arrNums = NumsInString.Select(int.Parse).ToArray();
+            int[] sumArrNums = new int[arrNums.Length];
+
+            for (int i = 0; i < k; i++)
+            {
+                int lastElem = arrNums[arrNums.Length - 1];
+                for (int y = arrNums.Length-1; y > 0; y--)
+                {
+                    arrNums[y] = arrNums[y - 1];
+                }
+                arrNums[0] = lastElem;
+                for (int z = 0; z < arrNums.Length; z++)
+                {
+                    sumArrNums[z] += arrNums[z];
+                }
+            }
+            foreach (var item in sumArrNums)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 06.10.17 - Arrays - Exercises/03. Fold and Sum/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int dataSmallLength = (data.Length / 2) / 2;
+            int[] dataBegining = new int[dataSmallLength];
+            for (int i = 0; i < dataSmallLength; i++)
+            {
+                dataBegining[i] = data[i];
+            }
+            Array.Reverse(dataBegining);
+            int count = 0;
+            int[] dataEnd = new int[dataSmallLength];
+            for (int i = data.Length-1; i >= data.Length / 2 + dataSmallLength; i--)
+            {
+                dataEnd[dataEnd.Length-1 - count] = data[i];
+                count++;
+            }
+            Array.Reverse(dataEnd);
+            int countMiddle = 0;
+            int[] dataMiddle = new int[data.Length / 2];
+            for (int i = dataSmallLength; i < data.Length - dataSmallLength; i++)
+            {
+                dataMiddle[countMiddle] = data[i];
+                countMiddle++;
+            }
+
+            int countSum = 0;
+            for (int i = 0; i < dataMiddle.Length; i++)
+            {
+                if (i < dataSmallLength)
+                {
+                    dataMiddle[i] += dataBegining[i];
+                }
+                else
+                {
+                    dataMiddle[i] += dataEnd[countSum];
+                    countSum++;
+                }
+            }
+            
+            foreach (var item in dataMiddle)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
