f5cc627b8324be0e5f4c87c8968b33f5693e97ef
daqn <daqn_5@abv.bg>
2017-10-17 17:40:42 +0300

    lab

---
+++ 12-10-17 DictionariesLambdaAndLINQ - Lab/03. Min, Max, Sum, Average/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Min__Max__Sum__Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> data = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                data.Add(number);
+            }
+            Console.WriteLine($"Sum = {data.Sum()}");
+            Console.WriteLine($"Min = {data.Min()}");
+            Console.WriteLine($"Max = {data.Max()}");
+            Console.WriteLine($"Average = {data.Average()}");
+        }
+    }
+}
