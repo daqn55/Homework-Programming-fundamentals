132aed5d195f2bbb1d07d0db36ab30e3ae39c03d
daqn <daqn_5@abv.bg>
2017-10-26 19:27:06 +0300

    Lab

---
+++ Regular Expressions (RegEx) - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\+359([ -])2\1[\d]{3}\1[\d]{4}\b";
+
+            Regex regex = new Regex(pattern);
+            MatchCollection result = regex.Matches(input);
+            List<string> phones = new List<string>();
+            foreach (Match m in result)
+            {
+                phones.Add(m.Value);
+            }
+
+            Console.WriteLine(string.Join(", ", phones));
+        }
+    }
+}
---
+++ Regular Expressions (RegEx) - Lab/03. Match Hexadecimal Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _03.Match_Hexadecimal_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\b(?:0x)?[0-9A-F]+\b";
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+            List<string> result = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                result.Add(match.Value);
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
