0fc3af3079fad9e991d0b4db740bac77e7d5d848
daqn <daqn_5@abv.bg>
2017-10-28 12:39:41 +0300

    homework

---
+++ Dictionaries, Lambda and LINQ -Exercises/06. User Logs/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _06.User_Logs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            SortedDictionary<string, Dictionary<string, int>> multyDict = new SortedDictionary<string, Dictionary<string, int>>();
+            while (input != "end" && input != "End" && input != "END")
+            {
+                string[] splitInput = input.Split(new char[] {'=', ' '});
+                if (splitInput.Length == 6)
+                {
+                    if (multyDict.Keys.Contains(splitInput[5]) && multyDict[splitInput[5]].Keys.Contains(splitInput[1]))
+                    {
+                        multyDict[splitInput[5]][splitInput[1]]++;
+                    }
+                    else
+                    {
+                        Dictionary<string, int> insaitDict = new Dictionary<string, int>();
+                        if (multyDict.Keys.Contains(splitInput[5]))
+                        {
+                            multyDict[splitInput[5]].Add(splitInput[1], 1);
+                        }
+                        else
+                        {
+                            insaitDict.Add(splitInput[1], 1);
+                            multyDict.Add(splitInput[5], insaitDict);
+                        }
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in multyDict)
+            {
+                int count = 0;
+                Console.WriteLine(item.Key + ": ");
+                foreach (var ip in item.Value)
+                {
+                    if (item.Value.Count-1 == count)
+                    {
+                        Console.Write(ip.Key + " => " + ip.Value + ".");
+                    }
+                    else
+                    {
+                        Console.Write(ip.Key + " => " + ip.Value + ", ");
+                    }
+                    count++;
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Strings and Text Processing - Exercises/07. Multiply big number/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Multiply_big_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().ToCharArray().Select(x => x.ToString()).Select(int.Parse).ToArray();
+            int numToMultiply = int.Parse(Console.ReadLine());
+
+            List<int> result = new List<int>();
+            if (numToMultiply == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (numToMultiply > 0 && numToMultiply < 10)
+            {
+                int memoryNum = 0;
+                for (int i = input.Length-1; i >= 0; i--)
+                {
+                    int sum = 0;
+                    sum = ((input[i] * numToMultiply) + memoryNum) % 10;
+                    memoryNum = (input[i] * numToMultiply + memoryNum) / 10;
+                    result.Add(sum);
+                }
+                if (memoryNum > 0 && memoryNum < 10)
+                {
+                    result.Add(memoryNum);
+                }
+                result.Reverse();
+                Console.WriteLine(string.Join("", result).TrimStart('0'));
+            }
+        }
+    }
+}
