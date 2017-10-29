d32954a86123bd260c2df49b29a43bf9538321e5
daqn <daqn_5@abv.bg>
2017-10-29 16:05:58 +0200

    homework

--- Regular Expressions (RegEx) - Exercises/01. Extract Emails/Program.cs
+++ Regular Expressions (RegEx) - Exercises/01. Extract Emails/Program.cs
@@ -12,7 +12,7 @@ namespace _01.Extract_Emails
         static void Main(string[] args)
         {
             string input = Console.ReadLine();
-            string pattern = @"[^-\._ ][a-z\._-]+\w+\w+@\w+.\w+\.[a-z]+";
+            string pattern = @"(?<![\-_.a-z0-9])[a-zA-Z0-9]+[\.|\-|_|a-z][a-z]+@[a-z]+[\-|a-z]+\.[a-z.]+\b";
             Regex regex = new Regex(pattern);
 
             MatchCollection match = regex.Matches(input);
---
+++ Regular Expressions (RegEx) - Exercises/04. Weather/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _04.Weather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([A-Z][A-Z])([0-9]+.[0-9]+)([A-Za-z]+)(?=\|)";
+            Regex regex = new Regex(pattern);
+            string input = Console.ReadLine();
+            Dictionary<string, Dictionary<double, string>> citiesWeather = new Dictionary<string, Dictionary<double, string>>();
+
+            while (input != "end")
+            {
+                MatchCollection match = regex.Matches(input);
+                foreach (Match item in match)
+                {
+                    if (citiesWeather.ContainsKey(item.Groups[1].Value))
+                    {
+                        citiesWeather[item.Groups[1].Value].Clear();
+                        citiesWeather[item.Groups[1].Value].Add(double.Parse(item.Groups[2].Value), item.Groups[3].Value);
+                    }
+                    else
+                    {
+                        Dictionary<double, string> nestetDict = new Dictionary<double, string>();
+                        nestetDict.Add(double.Parse(item.Groups[2].Value), item.Groups[3].Value);
+                        citiesWeather.Add(item.Groups[1].Value, nestetDict);
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var item in citiesWeather.OrderBy(x => x.Value.Keys.First()))
+            {
+                Console.Write(item.Key + " => ");
+                Console.WriteLine($"{item.Value.Keys.First():f2}" + " => " + item.Value.Values.First());
+            }
+        }
+    }
+}
