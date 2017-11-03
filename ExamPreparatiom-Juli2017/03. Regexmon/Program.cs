bf61f9d8fe9970c47fcfb1dc3bf52d6ee1cd2fe2
daqn <daqn_5@abv.bg>
2017-11-03 19:07:59 +0200

    ExamPreparatiom

--- ExamPreparatiom-Juli2017/03. Regexmon/Program.cs
+++ ExamPreparatiom-Juli2017/03. Regexmon/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace _03.Regexmon
@@ -10,7 +11,30 @@ namespace _03.Regexmon
     {
         static void Main(string[] args)
         {
-            string
+            string input = Console.ReadLine();
+
+            string didiAndBojo = @"([^-a-zA-Z]+)|([a-zA-Z]+-[a-zA-z]+)\b";
+
+            List<KeyValuePair<string, string>> matches = new List<KeyValuePair<string, string>>();
+            foreach (Match didi in Regex.Matches(input, didiAndBojo))
+            {
+                matches.Add(new KeyValuePair<string, string>("didi", didi.Groups[1].Value));
+                matches.Add(new KeyValuePair<string, string>("bojo" ,didi.Groups[2].Value));
+            }
+            int count = 1;
+            foreach (var item in matches.Where(x => x.Value.Length > 0))
+            {
+                if (item.Key == "didi" && count % 2 == 1)
+                {
+                    Console.WriteLine(item.Value.Trim());
+                    count++;
+                }
+                if (item.Key == "bojo" && count % 2 == 0)
+                {
+                    Console.WriteLine(item.Value.Trim());
+                    count++;
+                }
+            }
         }
     }
 }
---
+++ ExamPreparatiom-Juli2017/04. Pokemon Evolution/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Pokemon_Evolution
+{
+    class PokemonInfo
+    {
+        public string PokemonName { get; set; }
+        public List<KeyValuePair<string, int>> TypeAndIndex { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PokemonInfo> pokemonsData = new List<PokemonInfo>();
+            
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "wubbalubbadubdub")
+                {
+                    break;
+                }
+
+                string[] data = input.Split(new[] { " -> " }, StringSplitOptions.RemoveEmptyEntries);
+                PokemonInfo pokemonInfo = new PokemonInfo();
+                pokemonInfo.TypeAndIndex = new List<KeyValuePair<string, int>>();
+
+                if (data.Length == 1)
+                {
+                    foreach (var item in pokemonsData.Where(x => x.PokemonName.Contains(data[0])))
+                    {
+                        Console.WriteLine($"# {item.PokemonName}");
+                        foreach (var pokemon in item.TypeAndIndex)
+                        {
+                            Console.WriteLine($"{pokemon.Key} <-> {pokemon.Value}");
+                        }
+                    }
+                }
+                else
+                {
+                    if (pokemonsData.Any(x => x.PokemonName.Equals(data[0])))
+                    {
+                        foreach (var pokemon in pokemonsData.Where(x => x.PokemonName.Equals(data[0])))
+                        {
+                            pokemon.TypeAndIndex.Add(new KeyValuePair<string, int>(data[1], int.Parse(data[2])));
+                        }
+                    }
+                    else
+                    {
+                        pokemonInfo.PokemonName = data[0];
+                        pokemonInfo.TypeAndIndex.Add(new KeyValuePair<string, int>(data[1], int.Parse(data[2])));
+                        pokemonsData.Add(pokemonInfo);
+                    }
+                }
+            }
+
+            foreach (var item in pokemonsData)
+            {
+                Console.WriteLine($"# {item.PokemonName}");
+                foreach (var pokemon in item.TypeAndIndex.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"{pokemon.Key} <-> {pokemon.Value}");
+                }
+            }
+        }
+    }
+}
